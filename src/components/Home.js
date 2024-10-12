6a3e79cccd95fd257082bf35d2771ec4ec6cc086
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-12 09:24:30 +0530

    Dashboard files and folder structure

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopBar from './TopBar'
+import Dashboard from './Dashboard'
+function home() {
+  return (
+   <>
+   <TopBar />
+   <Dashboard />
+   </>
+  )
+}
+
+export default home
